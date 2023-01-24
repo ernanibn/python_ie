9e4aaf9ace86f84749f5457d928054c27ad104c6
ernanibn <ernanibn@gmail.com>
2023-01-24 00:21:00 -0300

    Third version

--- plot.py
+++ plot.py
@@ -3,21 +3,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = pd.read_csv('./database/netflix_titles.csv')
-# print("Before:")
-# print(data[['type','release_year']])
+print("Before:")
+print(data[['type','release_year']])
 
 data = data.loc[data['release_year'].isin([*range(2015, 2020)]), ['type',
 'release_year']].copy()
 data.dropna(inplace=True)
 data['release_year'] = data['release_year'].astype('int')
-# print("After:")
-# print(data)
+print("After:")
+print(data)
+
+
+
 
 cross_tab_prop = pd.crosstab(index=data['release_year'],
                              columns=data['type'],
                              normalize="index")
+
 cross_tab = pd.crosstab(index=data['release_year'],
                         columns=data['type'])
+
+
+data = pd.read_csv('./database/treatment.csv', index_col=0)
+
+# print(data.index)
+# print(data.columns)
+# cross_tab_prop = pd.crosstab(index=data.index,
+#                              columns=data.columns,
+#                              normalize="index")
+
+# cross_tab = pd.crosstab(index=data.index,
+#                         columns=data.columns)
+
+# print(cross_tab_prop)
 cross_tab_prop.plot(kind='bar', 
                     stacked=True, 
                     colormap='tab10', 
@@ -30,7 +48,7 @@ plt.ylabel("Proportion")
 for n, x in enumerate([*cross_tab.index.values]):
     for (proportion, y_loc) in zip(cross_tab_prop.loc[x],
                                    cross_tab_prop.loc[x].cumsum()):
-                
+
         plt.text(x=n - 0.17,
                  y=y_loc,
                  s=f'{np.round(proportion * 100, 1)}%', 
